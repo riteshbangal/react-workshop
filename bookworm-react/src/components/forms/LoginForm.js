1865f7b36bc923d10cc91609cf72042a37e4e923
Ritesh Bangal <rbangal@sapient.com>
2017-09-10 01:41:19 +0530

    login with client side validation

--- bookworm-react/src/components/forms/LoginForm.js
+++ bookworm-react/src/components/forms/LoginForm.js
@@ -1,15 +1,80 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Validator from "validator";
+
+import { Form, Button, Segment } from 'semantic-ui-react';
+
+import InlineError from "../messages/InlineError";
 
 class LoginForm extends React.Component {
 
+  state = {
+    data: {
+      email: "",
+      password: ""
+    },
+    loading: false,
+    errors: {}
+  };
+
+  onChange = e =>
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
+
+  onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+    if (Object.keys(errors).length === 0) {
+      this.setState({ loading: true });
+      this.props.submit(this.state.data);
+    }
+  };
+
+  validate = data => {
+    const errors = {};
+    if (!Validator.isEmail(data.email)) errors.email = "Invalid email";
+    if (!data.password) errors.password = "password can't be blank.";
+    return errors;
+  };
+
   render() {
+    const { data, errors, loading } = this.state;
 
     return (
-      <div> 
-        Login Form
-      </div>
+      <Form onSubmit={this.onSubmit} loading={loading}>
+        <Segment stacked padded>
+          <Form.Field error={!!errors.email}>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="E-mail address"
+              value={data.email}
+              onChange={this.onChange}
+            />
+            {errors.email && <InlineError text={errors.email} />}
+          </Form.Field>
+          <Form.Field error={!!errors.password}>
+           <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              value={data.password}
+              onChange={this.onChange}
+            />
+            {errors.password && <InlineError text={errors.password} />}
+          </Form.Field>
+          <Button primary color='teal' fluid size='large'>Login</Button>
+        </Segment>
+      </Form>
     );
   }
 }
+
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired
+};
 
 export default LoginForm;
--- bookworm-react/src/components/pages/LoginPage.js
+++ bookworm-react/src/components/pages/LoginPage.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Grid, Header, Image, Message, Divider } from 'semantic-ui-react';
 
 import LoginForm from '../forms/LoginForm';
 
-const LoginPage = () => (
-  <div>
-    <h2>Login Page</h2>
-    <LoginForm />
-  </div>
-);
+class LoginPage extends React.Component {
+
+  submit = data => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div className="login-form">
+        <style>
+          {`
+            body > div,
+            body > div > div,
+            body > div > div > div.login-form {
+              height: 100%;
+            }
+        ` }
+        </style>
+        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+              <Header as='h2' color='teal' textAlign='left'>
+                <Image src='./logo.png'/ >
+                {' '}Log-in to your account
+                <Divider horizontal/>
+                <LoginForm submit={this.submit} />
+              </Header>
+              <Message>
+                New to us? <Link to="/">Sign Up</Link>
+              </Message>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 export default LoginPage;
