4042fd0aaeb3b505cb4722c3d038953ef97ae4ae
Ritesh Bangal <rbangal@sapient.com>
2017-09-10 00:06:34 +0530

    Initial setup for bookworm-react application

---
+++ bookworm-react/src/components/forms/LoginForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class LoginForm extends React.Component {
+
+  render() {
+
+    return (
+      <div> 
+        Login Form
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
---
+++ bookworm-react/src/components/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import LoginForm from '../forms/LoginForm';
+
+const LoginPage = () => (
+  <div>
+    <h2>Login Page</h2>
+    <LoginForm />
+  </div>
+);
+
+export default LoginPage;
