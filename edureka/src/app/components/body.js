b74bd1571e8a05fcb2507cf36a39689aadf20c79
Ritesh Bangal <rbangal@sapient.com>
2017-09-12 12:46:01 +0530

    Call Rest GET API

--- edureka/src/app/components/body.js
+++ edureka/src/app/components/body.js
@@ -2,12 +2,39 @@ import React from "react";
 
 export class Body extends React.Component {
 
+    constructor() {
+		super();
+		this.state = {
+			authorNames: []
+		};
+    }
+    
+    fetchAuthors() {
+        
+        const { authorNames } = this.state
+        var name = '';
+
+        const url = "https://randomuser.me/api/?results=10";
+        fetch(url).then((response) => response.json())
+                  .then(function(data){
+                      let authors = data.results;
+                      authors.map(function(author ) {
+                        name = author.name.first + " " + author.name.last;
+                        console.log("Author Name: " + name);
+                        
+                      })
+                  })
+    }
+
     render() {
-        var text = "Inside Body!";
-        console.log("Body | this is console log.");
+        const { authorName } = this.state	
+        console.log("Author Name: " + authorName);
+
+        var text = "Authors API Call";
+        //console.log("Body | this is console log.");
         return (
             <div className="container">
-                <p>Body Text: {text}</p>
+                {text} {' : '}<button onClick={() => this.fetchAuthors()}>Get Authors</button>
             </div>
         );
     }
